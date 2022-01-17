8e84c1cb6c2fd38de40f876d49425a2ee28e76f9
techbret <bretljohnson0@gmail.com>
2022-01-16 20:20:15 -0500

    updated finished project

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ class Media {
     constructor(title) {
         this._title = title;
         this._isCheckedOut = false;
-        this._ratings = 0;
+        this._ratings = [];
     }
 
     get title() {
@@ -16,4 +16,82 @@ class Media {
     get ratings() {
         return this._ratings;
     }
+
+    toggleCheckOutStatus() {
+        this._isCheckedOut = !this._isCheckedOut;
+    }
+
+    getAverageRating() {
+        const reducer = (previusValue, currentValue) => previusValue + currentValue;
+        return this._ratings.reduce(reducer) / (this._ratings.length)
+    }
+
+    addRating(num) {
+        this._ratings.push(num);
+    }
 }
+
+class Book extends Media {
+    constructor(author, title, pages) {
+        super(title);
+        this._author = author;
+        this._pages = pages;
+    }
+
+    get author() {
+        return this._author;
+    }
+
+    get pages() {
+        return this._pages;
+    }
+}
+
+class Movie extends Media {
+    constructor(director, title, runTime) {
+        super(title);
+        this._director = director;
+        this._runTime = runTime;
+    }
+
+    get director() {
+        return this._director;
+    }
+
+    get runTime() {
+        return this._runTime;
+    }
+}
+
+class CD extends Media {
+    constructor(artist, title, songs) {
+        super(title);
+        this._artist = artist;
+        this._songs = songs;
+    }
+
+    get artist() {
+        return this._artist;
+    }
+
+    get songs() {
+        return this._songs;
+    }
+}
+
+const historyOfEverything = new Book('Bill Bryson', 'A Short History of nearly Everything', 544);
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(historyOfEverything.getAverageRating())
+
+const speed = new Movie('Jan de Bont', 'Speed', 116);
+speed.toggleCheckOutStatus();
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+console.log(speed.getAverageRating())
+
+
